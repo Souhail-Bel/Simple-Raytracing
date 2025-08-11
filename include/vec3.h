365c683e9dd7cb05f8083a9718d33ee9e392bf44
Souhail-Bel <robotboy976@gmail.com>
2025-08-11 14:06:02 +0100

    Minor vec3 changes

--- include/vec3.h
+++ include/vec3.h
@@ -17,6 +17,10 @@ class vec3 {
 		double y() const {return e[1];}
 		double z() const {return e[2];}
 		
+		// Indiv elements
+		double operator[](int i) const {return e[i];}
+		double& operator[](int i){return e[i];}
+		
 		vec3 operator-() const {
 			return vec3(-e[0], -e[1], -e[2]);
 		}
@@ -84,7 +88,7 @@ inline vec3 cross(const vec3& u, const vec3& v){
 				u.e[0]*v.e[1] - u.e[1]*v.e[0]);
 }
 
-inline vec3 normalize(const vec3& v) {
+inline vec3 normalized(const vec3& v) {
 	return v / v.len();
 }
 
