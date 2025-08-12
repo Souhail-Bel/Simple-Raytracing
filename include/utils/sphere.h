ab506645229e7c604c827f7d465f50b6338c36af
Souhail-Bel <robotboy976@gmail.com>
2025-08-12 22:12:42 +0100

    Added sphere definition

--- include/utils/sphere.h
+++ include/utils/sphere.h
@@ -6,4 +6,45 @@
 
 // Sphere to inherit from hittable interface
 
+class Sphere : public IHittable {
+	private:
+		point3 center;
+		float radius;
+	
+	public:
+		Sphere(const point3& c, float r) : center(c), radius(std::fabsf(r)) {}
+		
+		bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+			vec3 OC = center - r.origin();
+	
+			// Solves quadratic equation for sphere
+			// b' is used because b is even
+			double a = r.direction().len_sqr();
+			double b_pr = dot(r.direction(), OC);
+			double c = OC.len_sqr() - radius * radius;
+			
+			double del = (b_pr*b_pr - a*c);
+			
+			if(del < 0)
+				return false;
+			
+			double sqrt_del = sqrt(del);
+			
+			// Root within range
+			double t = (b_pr - sqrt_del) / a;
+			if(t <= ray_tmin || t <= ray_tmax) {
+				t = (b_pr + sqrt_del) / a;
+				
+				if(t <= ray_tmin || t <= ray_tmax)
+					return false;
+			}
+			
+			rec.t = t;
+			rec.p = r.at(t);
+			rec.normal = (rec.p - center) / radius;
+			
+			return true;
+		}
+};
+
 #endif
