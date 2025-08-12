762dc834a44413168bb36f974a00ba9014eb694a
Souhail-Bel <robotboy976@gmail.com>
2025-08-12 10:14:49 +0100

    Mapped 1.0 to 255 for colours

--- include/color.h
+++ include/color.h
@@ -9,10 +9,10 @@ using color = vec3;
 
 // ARGB8888
 inline uint32_t get_color(const color& pixel_color){
-	// Mapping 1.0 to 256
-	uint8_t R = pixel_color.x()*255.99;
-	uint8_t G = pixel_color.y()*255.99;
-	uint8_t B = pixel_color.z()*255.99;
+	// Mapping 1.0 to 255
+	uint8_t R = pixel_color.x()*255;
+	uint8_t G = pixel_color.y()*255;
+	uint8_t B = pixel_color.z()*255;
 	return (R << 16) | (G << 8) | B;
 }
 
