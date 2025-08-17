a10f18a02f456c7e66920e2e9a69ed233ca4fdbc
Souhail-Bel <robotboy976@gmail.com>
2025-08-17 20:27:12 +0100

    Reduced bvh.h size via lambda function as comparator

--- include/bvh.h
+++ include/bvh.h
@@ -20,21 +20,6 @@ class BVH_node : public IHittable {
 			auto b_axis_interval = b -> bounding_box().axis_interval(axis_index);
 			return a_axis_interval.min < b_axis_interval.min;
 		}
-		
-		static bool box_x_compare(
-			const shared_ptr<IHittable> a,
-			const shared_ptr<IHittable> b
-		) { return box_compare(a, b, 0); }
-		
-		static bool box_y_compare(
-			const shared_ptr<IHittable> a,
-			const shared_ptr<IHittable> b
-		) { return box_compare(a, b, 1); }
-		
-		static bool box_z_compare(
-			const shared_ptr<IHittable> a,
-			const shared_ptr<IHittable> b
-		) { return box_compare(a, b, 2); }
 	
 	public:
 		BVH_node(hittable_list list) : BVH_node(list.objects, 0, list.objects.size()) {
@@ -49,9 +34,9 @@ class BVH_node : public IHittable {
 			
 			int axis = bbox.longest_axis();
 			
-			auto comparator = (axis == 0) ? box_x_compare
-					: (axis == 1) ? box_y_compare
-					: box_z_compare;
+			auto comparator = [axis](const shared_ptr<IHittable>& a, const shared_ptr<IHittable>& b) {
+				return box_compare(a, b, axis);
+			};
 			
 			size_t object_span = end - start;
 			
